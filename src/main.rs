7649b62a9e56fa583d121de02374e4f2d9325325
Nick Santana <nick@mobilecoin.com>
2024-08-23 08:53:32 -0700

    Finished fondling.
    adding migration logic

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,13 @@ use diesel::pg::PgConnection;
 use std::env;
 use std::thread;
 use schema::simple_table;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 
 fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
-
-fn update_counter(conn: &mut PgConnection, user_name: &str, value: i32) -> QueryResult<usize> {
-    diesel::update(simple_table::table)
-        .filter(simple_table::name.eq(user_name))
-        .set(simple_table::counter.eq(value)).execute(conn)
 }
 
 fn insert_user(conn: &mut PgConnection, user_name: &str) {
@@ -24,12 +21,32 @@ fn insert_user(conn: &mut PgConnection, user_name: &str) {
         .expect("Error inserting user");
 }
 
+fn update_counter(conn: &mut PgConnection, user_name: &str, value: i32) -> QueryResult<usize> {
+    diesel::update(simple_table::table)
+        .filter(simple_table::name.eq(user_name))
+        .set(simple_table::counter.eq(value))
+        .execute(conn)
+}
+
+fn lock_row(conn: &mut PgConnection, user_name: &str) -> QueryResult<()> {
+    diesel::sql_query("SELECT * FROM simple_table WHERE name = $1 FOR UPDATE")
+        .bind::<diesel::sql_types::VarChar, _>(user_name)
+        .execute(conn)
+        .map(|_| ())
+}
+
+fn for_update(conn: &mut PgConnection) {
+    simple_table::table.for_update().execute(conn).expect("Error locking table");
+}
+
 fn run(user_name: &str) {
     let mut connection = establish_connection();
     insert_user(&mut connection, user_name);
     let mut counter = 0;
     loop {
         let result = connection.build_transaction().serializable().run(|conn| {
+            lock_row(conn, user_name)?;
+            // for_update(conn);
             update_counter(conn, user_name, counter)
         });
         println!("Result for {user_name}: {:?}", result);
@@ -38,6 +55,11 @@ fn run(user_name: &str) {
 }
 
 fn main() {
+    {
+        let mut connection = establish_connection();
+        connection.run_pending_migrations(MIGRATIONS).expect("Failed to run migrations");
+    }
+
     let handle1 = thread::spawn(move || {
         run("User1");
     });
