c9c179f154482b413ddbace1ee36d33139697f16
Nick Santana <nick@mobilecoin.com>
2024-08-26 15:04:27 -0700

    I don't give a damn 'bout my reputation
    fix the migration

--- migrations/2024-08-26-171628_insert_contention/down.sql
+++ migrations/2024-08-26-171628_insert_contention/down.sql
@@ -1,6 +1,6 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE serial_key_table;
-DROP TABLE uuid_key_table;
 DROP TABLE unique_column_table;
 DROP TABLE unique_string_column_table;
 DROP TABLE foreign_key_column_table;
+DROP TABLE uuid_key_table;
