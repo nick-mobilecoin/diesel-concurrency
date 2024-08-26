dccd529ccbfbd650e541cdaf7b9d7be6309a19c1
Nick Santana <nick@mobilecoin.com>
2024-08-26 09:53:59 -0700

    Renaming the table to be more in line with the example

--- migrations/2024-08-23-145250_update_row/up.sql
+++ migrations/2024-08-23-145250_update_row/up.sql
@@ -1,5 +1,5 @@
--- Your SQL goes here
-CREATE TABLE simple_table (
+-- A table for showing concurrency problems when updating row values
+CREATE TABLE update_row (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     counter INTEGER NOT NULL DEFAULT 0
